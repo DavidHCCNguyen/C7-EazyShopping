4d53a0b3056b934d1604c901122acaf3058388f3
DavidHCCNguyen <creativespecials@gmail.com>
2023-04-30 18:32:38 +0930

    Getting newest stuff

--- signup/signupscript.js
+++ signup/signupscript.js
@@ -1,19 +1,18 @@
 window.addEventListener('load', function() {
-    const form = document.querySelector('.form-signin');
-  
-    form.addEventListener('submit', function (e) {
-      e.preventDefault();
-  
-      // Get email and password values from form inputs
-      const email = document.querySelector('#inputEmail').value;
-      const password = document.querySelector('#inputPassword').value;
-  
-      // Save email and password values to local storage
-      localStorage.setItem('email', email);
-      localStorage.setItem('password', password);
-  
-      // Redirect user to a different page or display a message
-      // to indicate that sign-up was successful
-    });
+  const form = document.querySelector('.sign-in-form');
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Get email and password values from form inputs
+    const email = document.querySelector('#sign-in-form-username').value;
+    const password = document.querySelector('#sign-in-form-password').value;
+
+    // Save email and password values to local storage
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+
+    // Redirect user to a different page or display a message
+    // to indicate that sign-up was successful
   });
-  
+});
