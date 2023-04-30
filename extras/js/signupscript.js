8676c18c2cb482dd9f98e082dd5d6140cc672594
DavidHCCNguyen <creativespecials@gmail.com>
2023-04-30 21:28:03 +0930

    Changing login and sign up html so it knows where css and js are

--- extras/js/signupscript.js
+++ extras/js/signupscript.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:signup/signupscript.js
 // Get the form and the input fields
 const form = document.querySelector('form');
 const usernameInput = document.querySelector('#sign-up-form-username');
@@ -20,23 +19,3 @@ form.addEventListener('submit', (event) => {
   // Redirect the user to another page in the same directory
   window.location.href = "../login.html";
 });
-=======
-window.addEventListener('load', function() {
-    const form = document.querySelector('.form-signin');
-  
-    form.addEventListener('submit', function (e) {
-      e.preventDefault();
-  
-      // Get email and password values from form inputs
-      const email = document.querySelector('#inputEmail').value;
-      const password = document.querySelector('#inputPassword').value;
-  
-      // Save email and password values to local storage
-      localStorage.setItem('email', email);
-      localStorage.setItem('password', password);
-  
-      // Redirect user to a different page or display a message
-      // to indicate that sign-up was successful
-    });
-  });
->>>>>>> 3ad94c4af3199dbdd79d106e4591d8588d21c3a6:extras/js/signupscript.js
