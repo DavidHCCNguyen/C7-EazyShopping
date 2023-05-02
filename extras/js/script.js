847479c3d1a9198eb27b65e4c61b9e4035da8b1e
retnodamayanti <mayahere5@gmail.com>
2023-05-02 23:08:36 +0930

    updated local storage and added remove button and its style

--- extras/js/script.js
+++ extras/js/script.js
@@ -30,133 +30,83 @@ fetch(fakeStoreUrl)
     }
   });
 
-    addToCartButton.forEach(function(button) {
-        button.addEventListener('click', function() {
-            var product = {
-                name: button.dataset.productName,
-                price: button.dataset.productPrice,
-            };
-            productCartItems.push(product);
-            updateCheckoutSummary();
-        });
-    
+  addToCartButton.forEach(function(button) {
+    button.addEventListener('click', function() {
+      var product = {
+        name: button.dataset.productName,
+        price: button.dataset.productPrice,
+      };
+      // Load data from local storage
+      const storedItems = JSON.parse(localStorage.getItem('productCartItems'));
+      if (storedItems && storedItems.length > 0) {
+        // Append the new product to the existing array
+        productCartItems = storedItems;
+        productCartItems.push(product);
+      } else {
+        // Create a new array with the new product
+        productCartItems = [product];
+      }
+      localStorage.setItem('productCartItems', JSON.stringify(productCartItems)); // Save the updated array to local storage
+      updateCheckoutSummary();
     });
+  });
+  
   
   function updateCheckoutSummary() {
-    const cartList = document.querySelector(".checkout-summary-item");
-    cartList.innerHTML = "";
+    const cartList = document.querySelector('.checkout-summary-item');
+    cartList.innerHTML = '';
   
     // Load data from local storage
-    const storedItems = JSON.parse(localStorage.getItem("productCartItems"));
+    const storedItems = JSON.parse(localStorage.getItem('productCartItems'));
     if (storedItems && storedItems.length > 0) {
       productCartItems = storedItems;
     }
   
-    productCartItems.forEach(function (item, index) {
-      const li = document.createElement("li");
-      const name = document.createElement("p");
+    productCartItems.forEach(function(item, index) {
+      const li = document.createElement('li');
+      const name = document.createElement('p');
       name.textContent = item.name;
       li.appendChild(name);
-      const price = document.createElement("p");
-      price.textContent = "$" + item.price;
+      const price = document.createElement('p');
+      price.textContent = '$' + item.price;
       li.appendChild(price);
   
       // Create a remove button for each item
-      const removeBtn = document.createElement("button");
-      removeBtn.textContent = "Remove";
-      removeBtn.addEventListener("click", function () {
+      const removeBtn = document.createElement('button');
+      removeBtn.textContent = 'Remove';
+      removeBtn.addEventListener('click', function() {
         productCartItems.splice(index, 1); // Remove the item from the productCartItems array
-        localStorage.setItem("productCartItems", JSON.stringify(productCartItems)); // Save the updated array to local storage
+        localStorage.setItem('productCartItems', JSON.stringify(productCartItems)); // Save the updated array to local storage
         updateCheckoutSummary(); // Update the checkout summary display
       });
       li.appendChild(removeBtn);
   
       cartList.appendChild(li);
   
-      cartList.setAttribute("style", "display:block;");
-      price.setAttribute(
-        "style",
-        "font-size: 20px;font-family: 'Slabo 27px', serif; position:relative; left:400px; bottom:45px;"
-      );
-      name.setAttribute(
-        "style",
-        "font-size: 20px;font-family: 'Slabo 27px', serif;"
-      );
-      li.setAttribute("style", "width:fit-content;");
+      cartList.setAttribute('style', "display:block;");
+      price.setAttribute('style', "font-size: 20px;font-family: 'Slabo 27px', serif; position:relative; left:400px; bottom:50px;");
+      name.setAttribute('style', "font-size: 20px;font-family: 'Slabo 27px', serif; width: 350px;");
+      li.setAttribute('style', "width:fit-content;")
+      removeBtn.setAttribute('style', 
+      "background-color: #1779ba; border: none; color: white; padding: 5px 15px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px;");
     });
   
-    // Calculate the total price of products
-    let total = 0.0;
+    let total = 0.00;
+  
     for (var i = 0; i < productCartItems.length; i++) {
       total += parseFloat(productCartItems[i].price);
-      console.log(typeof productCartItems[i].price);
     }
   
-    // Display the total price in the cart
     cartTotals.textContent = "$" + total.toFixed(2);
     totalEl.textContent = "Total";
     cartList.appendChild(cartTotals);
     cartList.appendChild(totalEl);
   
-    cartTotals.setAttribute(
-      "style",
-      "font-weight:bold; font-size: 20px;font-family: 'Slabo 27px', serif; position:relative; left:400px;"
-    );
-    totalEl.setAttribute(
-      "style",
-      "font-size: 20px;font-family: 'Slabo 27px', serif; position:relative; bottom:45px;"
-    );
-    checkoutCart.setAttribute(
-      "style",
-      "display:inline-block; position:relative; left: 1200px; bottom:2000px;"
-    );
-  }
-    
-     function updateCheckoutSummary() {
-        const cartList = document.querySelector('.checkout-summary-item');
-        cartList.innerHTML = '';
-        
-        productCartItems.forEach(function(item) {
-            const li = document.createElement('li');
-            const name = document.createElement('p');
-            name.textContent = item.name;
-            li.appendChild(name);
-            const price = document.createElement('p');
-            price.textContent = '$' + item.price;
-            li.appendChild(price);
-            cartList.appendChild(li);
-            
-            cartList.setAttribute('style', "display:block;");
-            price.setAttribute('style', "font-size: 20px;font-family: 'Slabo 27px', serif; position:relative; left:400px; bottom:50px;");
-            name.setAttribute('style', "font-size: 20px;font-family: 'Slabo 27px', serif; width: 350px;");
-            li.setAttribute('style', "width:fit-content;")
-            
-        });
-        console.log(productCartItems);
-        let total = 0.00;
-    
-        for (var i = 0; i < productCartItems.length; i++) {
-    
-              total += parseFloat(productCartItems[i].price);
-              
-            
-        }
-       
-        console.log(total);
-        
-        
-        
-        cartTotals.textContent = "$" + total.toFixed(2);
-        totalEl.textContent = "Total";
-        cartList.appendChild(cartTotals);
-        cartList.appendChild(totalEl);
-        
-        
-        cartTotals.setAttribute('style', "font-weight:bold; font-size: 25px;font-family: 'Slabo 27px', serif; position:relative; left:400px;");
-        totalEl.setAttribute('style', "font-weight:bold; font-size: 25px;font-family: 'Slabo 27px', serif; position:relative; bottom:50px;");
-        checkoutCart.setAttribute('style', 'display:block;');
-    
-     };
+    cartTotals.setAttribute('style', "font-weight:bold; font-size: 25px;font-family: 'Slabo 27px', serif; position:relative; left:400px;");
+    totalEl.setAttribute('style', "font-weight:bold; font-size: 25px;font-family: 'Slabo 27px', serif; position:relative; bottom:50px;");
+    checkoutCart.setAttribute('style', 'display:block;');
+  };
+
 
 
      $( function() {
